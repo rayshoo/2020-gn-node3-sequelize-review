b33a660fc076015564a21e5cd448943e008d7579
dfnk5516 <rayshoo@naver.com>
2020-06-28 13:32:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## sequelize@6
+
+https://sequelize.org/master/manual/upgrade-to-v6.html
+
+## homework
+
+09.board 만들어서 04.의 db를 orm으로 만들어오기
+config.json 을 .js로 만들어오기
---
+++ models/Board.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define(
+    "Board",
+    {
+      // idx: {
+      //   type: Sequelize.INTEGER(11),
+      //   primarykey: true,
+      //   autoIncrement: true
+      // },
+      title: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      writer: {
+        type: Sequelize.STRING(20),
+      },
+      content: {
+        type: Sequelize.TEXT(),
+      },
+      rnum: {
+        type: Sequelize.INTEGER(11),
+        defaultValue: 0,
+      },
+    },
+    {
+      timestamp: true,
+      charset: "utf8",
+      tableName: "seqboard2",
+    }
+  );
+};
